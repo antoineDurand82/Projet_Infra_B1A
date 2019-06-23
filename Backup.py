db06a8f970912b839b1db2c74d179658663bb069
Sascha <sascha.salles@ynov.com>
2019-06-23 21:22:32 +0200

    Rajout de la list (test)

--- Backup.py
+++ Backup.py
@@ -17,13 +17,18 @@ entr3_var = StringVar()
 entr1 = Entry(can, textvariable = entr1_var)
 entr2 = Entry(can, textvariable = entr2_var)
 entr3 = Entry(can, textvariable = entr3_var)
+liste_bash = Listbox(can, width = 50)
 
+cmd = os.popen("./backup.sh list", 'r')
+liste_bash.insert(1, cmd)
 
 ### Fonctions
 
 def extract():
-	os.system("./backup.sh extract")
-	
+	global entr3_var
+	entr3_var = entr3_var.get()
+	os.system("./backup.sh extract %s" %(entr3_var))
+
 def save():
 	global entr1_var, entr2_var
 	entr1_var = entr1_var.get()
@@ -38,12 +43,14 @@ def delete():
 def deleteAll():
 	os.system("yes `echo YES` | ./backup.sh delete all")
 
+
 ### Buttons ###
 
 boutton_Save = Button(can, text = "Save", command = save)
 boutton_Delete = Button(can, text = "Delete", command = delete)
 boutton_Delete_all = Button(can, text = "Delete All", command = deleteAll)
 boutton_Extract = Button(can, text = "Extract", command = extract)
+
 
 ### Positions ###
 txt1.grid(row = 1, column = 1)
@@ -59,6 +66,7 @@ boutton_Delete.grid(row = 4, column = 5)
 
 boutton_Extract.grid(row = 4, column = 2)
 boutton_Delete_all.grid(row = 5, column = 5)
+liste_bash.grid(row = 6, column=3)
 
-can.grid(row = 5, column = 8, rowspan = 5)
+can.grid(row = 6, column = 8, rowspan = 5, padx=10, pady=10)
 fen.mainloop()
