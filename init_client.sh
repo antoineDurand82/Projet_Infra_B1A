b76406e6bb890c8aced535b2104242006df83b80
Antoine.D <antoine.durand@ynov.com>
2019-06-24 00:03:02 +0200

    modif save journalière client

--- init_client.sh
+++ init_client.sh
@@ -24,13 +24,13 @@ chmod +x auto.sh
 
 sudo echo "serveurnfs:/backup/ /backup nfs defaults,user,auto,noatime,bg 0 0">>/etc/fstab
 
-echo -e "\n\nVeuillez fournir le chemin complet jusqu'à votre dossier où vous voulez avoir une backup d'un dossier particulier' \nPar exemple '/home/antoine/Desktop'. Pensez à retirer le dernier '/' se trouvant après le dernier dossier"
+echo -e "\n\nVeuillez fournir le chemin complet jusqu'à votre dossier pour avoir une backup journalière de ce dernier' \nPar exemple '/home/antoine/Desktop'. Pensez à retirer le dernier '/' se trouvant après le dernier dossier"
 read cheminbackup
 
 echo -e "\n\nVeuillez fournir le chemin complet jusqu'à votre dossier contenant le fichier que vous venez de lancer"
 read cheminfichier
 
-sudo echo "sudo cp -r /backup $cheminbackup">>$cheminfichier/auto.sh
+sudo echo "yes | $cheminfichier/backup.sh save sauvegardeJournaliere_`date "+\%d.\%m.\%Y"` $cheminbackup">>$cheminfichier/auto.sh
 
 sudo mv $cheminfichier/auto.sh /etc/cron.daily/
 
