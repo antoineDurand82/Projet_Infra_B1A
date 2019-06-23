74f2658a5202caed207ec107c45685cd6eb2e040
Antoine.D <antoine.durand@ynov.com>
2019-06-23 21:24:16 +0200

    add auto save client

--- init_client.sh
+++ init_client.sh
@@ -19,9 +19,22 @@ sudo showmount -e serveurnfs
 
 sudo mkdir /backup
 
+touch auto.sh
+chmod +x auto.sh
+
 sudo echo "serveurnfs:/backup/ /backup nfs defaults,user,auto,noatime,bg 0 0">>/etc/fstab
 
-echo "Installation terminé, votre ordinateur va redémarrer dans 5 secondes"
+echo -e "\n\nVeuillez fournir le chemin complet jusqu'à votre dossier où vous voulez avoir une backup d'un dossier particulier' \nPar exemple '/home/antoine/Desktop'. Pensez à retirer le dernier '/' se trouvant après le dernier dossier"
+read cheminbackup
+
+echo -e "\n\nVeuillez fournir le chemin complet jusqu'à votre dossier contenant le fichier que vous venez de lancer"
+read cheminfichier
+
+sudo echo "sudo cp -r /backup $cheminbackup">>$cheminfichier/auto.sh
+
+sudo mv $cheminfichier/auto.sh /etc/cron.daily/
+
+echo "Installation terminée, votre ordinateur va redémarrer dans 5 secondes"
 
 sleep 5
 
