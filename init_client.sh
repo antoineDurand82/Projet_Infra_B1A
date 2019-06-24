61996cc1885dfbe9bb255c576cfcd78b23e20617
Antoine.D <43401777+antoineDurand82@users.noreply.github.com>
2019-06-24 19:57:12 +0200

    supr sudo

--- init_client.sh
+++ init_client.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sudo apt-get install nfs-common -y
+apt-get install nfs-common -y
 
-sudo apt-get install borgbackup -y
+apt-get install borgbackup -y
 
-sudo apt install python3-pip -y
+apt install python3-pip -y
 
-sudo apt-get install python3-tk -y
+apt-get install python3-tk -y
 
 chmod +x backup.sh
 chmod +wxr Backup.py
@@ -14,16 +14,16 @@ chmod +wxr Backup.py
 echo -e "\n\nVeuillez fournir l'adresse ip de votre serveur:"
 read ip_serveur
 
-sudo echo "$ip_serveur serveurnfs">>/etc/hosts
+echo "$ip_serveur serveurnfs">>/etc/hosts
 
-sudo showmount -e serveurnfs
+showmount -e serveurnfs
 
-sudo mkdir /backup
+mkdir /backup
 
 touch auto.sh
 chmod +x auto.sh
 
-sudo echo "serveurnfs:/backup/ /backup nfs defaults,user,auto,noatime,bg 0 0">>/etc/fstab
+echo "serveurnfs:/backup/ /backup nfs defaults,user,auto,noatime,bg 0 0">>/etc/fstab
 
 echo -e "\n\nVeuillez fournir le chemin complet jusqu'à votre dossier pour avoir une backup journalière de ce dernier' \nPar exemple '/home/antoine/Desktop'. Pensez à retirer le dernier '/' se trouvant après le dernier dossier"
 read cheminbackup
@@ -31,12 +31,9 @@ read cheminbackup
 echo -e "\n\nVeuillez fournir le chemin complet jusqu'à votre dossier contenant le fichier 'init_client.sh' que vous venez de lancer"
 read cheminfichier
 
-sudo echo "yes | $cheminfichier/backup.sh save sauvegardeJournaliere_`date "+\%d.\%m.\%Y"` $cheminbackup">>$cheminfichier/auto.sh
+echo "yes | $cheminfichier/backup.sh save sauvegardeJournaliere_`date "+\%d.\%m.\%Y"` $cheminbackup">>$cheminfichier/auto.sh
 
-sudo mv $cheminfichier/auto.sh /etc/cron.daily/
+mv $cheminfichier/auto.sh /etc/cron.daily/
 
-echo "Installation terminée, votre ordinateur va redémarrer dans 5 secondes"
+echo "Installation terminée."
 
-sleep 5
-
-sudo reboot
