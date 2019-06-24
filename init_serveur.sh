f74396966101662b3829eadf7931bef545408a3f
Antoine.D <43401777+antoineDurand82@users.noreply.github.com>
2019-06-24 19:40:58 +0200

    supr sudo

--- init_serveur.sh
+++ init_serveur.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-sudo apt install nfs-kernel-server -y
+apt install nfs-kernel-server -y
 
-sudo mkdir /backup
+mkdir /backup
 
 echo -e "\n\nVeuillez fournir l'adresse ip de votre client:"
 read ip_client
 
-sudo echo "$ip_client client">>/etc/hosts
+echo "$ip_client client">>/etc/hosts
 
-sudo echo "/backup client(rw,no_root_squash)">>/etc/exports
+echo "/backup client(rw,no_root_squash)">>/etc/exports
 
-sudo service nfs-kernel-server reload
+service nfs-kernel-server reload
 
-sudo service nfs-kernel-server restart
+service nfs-kernel-server restart
 
 touch auto.sh
 chmod +x auto.sh
@@ -24,8 +24,8 @@ read cheminbackup
 echo -e "\n\nVeuillez fournir le chemin complet jusqu'à votre dossier contenant le fichier 'init_serveur.sh' que vous venez de lancer"
 read cheminfichier
 
-sudo echo "sudo cp -r /backup $cheminbackup">>$cheminfichier/auto.sh
+echo "sudo cp -r /backup $cheminbackup">>$cheminfichier/auto.sh
 
-sudo mv $cheminfichier/auto.sh /etc/cron.daily/
+mv $cheminfichier/auto.sh /etc/cron.daily/
 
 echo -e "\n\nL'installion est terminée, bravo"
